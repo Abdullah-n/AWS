d37387da508915b96e596c9d1be10951629a6aa0
Abdullah-n <81876069+Abdullah-n@users.noreply.github.com>
2021-06-09 12:44:23 +0500

    Add files via upload

---
+++ myec2.py
@@ -0,0 +1,65 @@
+
+try:
+    import boto3
+    print("All Modules are Loaded ......")
+except Exception as e:
+    print("Some Modules are missings {}".format(e))
+
+
+class EC2Master(object):
+
+    def __init__(self):
+        self.client = boto3.client('ec2')
+
+    def stop(self, InstanceId = []):
+        """
+
+        :param InstanceId: List of Instance ID
+        :return:
+        """
+        response = self.client.stop_instances(
+            InstanceIds=InstanceId)
+        return response
+
+    def get_instanceid(self):
+        """
+        :return: List of Instance ID
+        """
+        id = []
+        response = self.client.describe_instances()
+
+        for reservation in response["Reservations"]:
+            for instance in reservation["Instances"]:
+                id.append(instance["InstanceId"])
+        return id
+
+    def start(self, InstanceId = []):
+        """
+
+        :param InstanceId: List of Instance ID
+        :return:
+        """
+        response = self.client.start_instances(
+            InstanceIds=InstanceId)
+        return response
+
+
+if __name__ == "__main__":
+    obj = EC2Master()
+
+    # Instance ID in a List
+    data = obj.get_instanceid()
+    print("Instance Id {}".format(data))
+
+    # start the EC2 Machine
+    obj.start(InstanceId=data)
+    print("Machine is Turned On")
+    
+    #stop the EC2 machine
+    obj.stop(InstanceId=data)
+    print("The ec2 is turned off")
+
+
+
+
+
